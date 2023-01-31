eeb00470a698a04e6dc3ad633c956aeb6a72e4e3
leesuyeon <lgh0385hh@gmail.com>
2023-01-31 13:27:00 +0900

    Start Project

--- js/main.js
+++ js/main.js
@@ -17,12 +17,12 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.setAttribute('placeholder', '')
 })
 
-
 /**
  * 페이지 스크롤에 따른 요소 제어
  */
 // 페이지 스크롤에 영향을 받는 요소들을 검색!
 const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
 // 페이지에 스크롤 이벤트를 추가!
 // 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
 window.addEventListener('scroll', _.throttle(function () {
@@ -33,6 +33,10 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display: 'none'
     })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
 
   // 페이지 스크롤 위치가 500px이 넘지 않으면.
   } else {
@@ -41,8 +45,22 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 1,
       display: 'block'
     })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
   }
-}, 300));
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
+
+
+
 
 // Visual 이미지 하나씩 띄우기
 const fadeEls = document.querySelectorAll('.visual .fade-in')
@@ -76,19 +94,68 @@ new Swiper('.promotion .swiper-container', {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next'
   }
+  });
+
+ new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,  //사이 사이 여백
+  slidesPerView: 5, //슬라이드 당 몇 개 보일지
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
   }
-);
+
+}) 
 
 const promotionEl = document.querySelector('.promotion');
-const promotionToggleBtn = document.querySelector('.toggle-promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
 promotionToggleBtn.addEventListener('click', function () {
-  isHidePromotion = !isHidePromotion
+  isHidePromotion = ! isHidePromotion
   if (isHidePromotion) {
+    // 숨김 처리!
     promotionEl.classList.add('hide');
   }
   else {
     promotionEl.classList.remove('hide');
   }
+  
+});
 
-})
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1, // 반복 재생
+    yoyo: true, // 거꾸로 재생
+    ease: "power1.inOut",
+    delay: random(0, delay)
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 가져옴
+      triggerHook: .8 //
+    })
+    .setClassToggle(spyEl, 'show') 
+    .addTo(new ScrollMagic.Controller());
+});
+
+
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear(); // 2023
